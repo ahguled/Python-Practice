8bb53a1af910b119c391a9eda66824d710fdc1cb
ahguled <ahguled11@egmail.com>
2022-06-17 12:44:51 -0400

    Added exception handling and checks. Updated CSV.

--- rds.py
+++ rds.py
@@ -1,50 +1,45 @@
 ## importing packages
 import mysql.connector
 import pandas as pd
-
-## Connection to MySQL: user and password credentials need to be used.
-connection= mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='fake_password'
-)
-
-##Setup for MySQL operations
-statement=connection.cursor()
-# HOW TO USE example ---->   statement.execute('SQL QUERY')
-
+try:
+    ## Connection to MySQL: user and password credentials need to be used.
+    print('Attempting to connect to MariaDB/MySQL')
+    connection= mysql.connector.connect(host="127.0.0.1", user="root", password="")
+    ##Setup for RDS operatiosn
+    statement=connection.cursor()
+except mysql.connector.errors.ProgrammingError:
+    print('Connection failed check instance connection and credentials.')
 ## CREATE DATABASE and USE
-statement.execute('CREATE DATABASE users')
-statement.execute('USE users')
-
-## Create Table
-statement.execute('''
-    CREATE TABLE user_info (
-        No INT PRIMARY KEY, 
-        Name_id VARCHAR(55), 
-        email VARCHAR(255)
-        )
-    ''')
-
+try:
+    statement.execute('CREATE DATABASE users;')
+    print('Database \'users\' was created successfully.')
+    statement.execute('USE users;')
+except mysql.connector.errors.DatabaseError:
+    print('Loading database \'users\'.')
+    statement.execute('USE users;')
+try:   
+    ## Create Table
+    statement.execute('CREATE TABLE user_info (No INT PRIMARY KEY,  Name_id VARCHAR(55), email VARCHAR(255));')
+except mysql.connector.errors.ProgrammingError:
+    print('Table exist already.')
+    check=True
+    while check:
+        choice=input("Drop table user_info that already exists and create new one? \nType \'yes\' or \'no\'.\n").upper()
+        if choice == 'YES':
+            #Drop table if exist
+            statement.execute('DROP TABLE IF EXISTS user_info;')
+            check=False
+        elif choice == 'NO':
+            check=False
+            pass
+        else:
+            print('Invalid input try again.')
 ## Importing CSV file via Pandas
-    ## may need to use absolute path for file location if you moved input folder.
 data=pd.read_csv(r'input\a.csv')
-df=pd.Dataframe(data)
-
-## INSERT VALUES into table using pandas
-for row in df.intertuples():
-    statement.execute('''
-    INSERT INTO user_info(No, Name_id, email) 
-    VALUES (?,?,?)
-    ''',
-    row.No,
-    row.Name_id,
-    row.email
-    )
-
-## Show results
-result=statement.execute('Select * from users')
-print(result)
-
+df=pd.DataFrame(data)
+## INSERT VALUES into table using pandas dataFrame object
+for row in df.itertuples():
+    statement.execute('INSERT INTO user_info(No, Name_id, email) VALUES (?,?,?), row.No, row.Name_id, row.email')    
+statement.close()
 connection.commit()
 connection.close()
